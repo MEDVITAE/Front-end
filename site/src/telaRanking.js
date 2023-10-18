4bf1dadc1dacbf9de9bbcbc9b6c8b529396f25a1
pedroafonso0728 <pedro.marcato@sptech.school>
2023-10-18 08:51:31 -0300

    Tela de visualização de agendamentos (Funcionario)

--- site/src/telaRanking.js
+++ site/src/telaRanking.js
@@ -1,5 +1,5 @@
 var xValues = ["Pedro", "Vinicios", "Mayara", "Maessio", "Tenório", "Paternezi", "Solange", "Daniel", "Sonia", "Monica"];
-var yValues = [55, 49, 44, 24, 40, 30, 23, 40, 50, 30];
+var yValues = [55, 49, 44, 24, 40, 30, 40, 47, 50, 40];
 var barColors = ["red", "green", "blue", "orange", "purple", "pink", "black", "Violet", "Teal", "gold"];
 
 new Chart("myChart", {
