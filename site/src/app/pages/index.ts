f7bde31c4f537b494758f3629904b4a409ad5dc5
DiegoCmoreira <diego.moreira@sptech.school>
2023-10-29 13:13:20 -0300

    Base para mapa

--- site/src/app/pages/index.ts
+++ site/src/app/pages/index.ts
@@ -13,7 +13,7 @@ export * from './login/Login';
 export * from './usuario/agenda/Agenamento';
 export * from './usuario/historico/Historico';
 export * from './usuario/mapa/Mapa';
-export * from './usuario/mapa/HospitalDetalhes'
+export * from './usuario/mapa/HospitalDetalhes';
 export * from './usuario/perfil/Perfil';
 export * from './usuario/quiz/Quiz';
 export * from './usuario/ranking/Ranking';
