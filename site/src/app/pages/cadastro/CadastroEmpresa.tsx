1f81fea5f2e1cc76abd59564b035aa548b9439b8
theviniciosgarcia <vinicios.fagundes@sptech.school>
2023-11-13 18:26:58 -0300

    Merge forçado com branch CadastroReactV

--- site/src/app/pages/cadastro/CadastroEmpresa.tsx
+++ site/src/app/pages/cadastro/CadastroEmpresa.tsx
@@ -1,53 +1,185 @@
-import '../../../html-css-template/css/homocentro.css'
+import "../../../html-css-template/css/homocentro.css";
+import { vetorImg } from "../../shared/components/imagens";
+import { vetorIcon } from "../../shared/components/imagens";
+
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Input } from "../../shared/components";
+import Swal from "sweetalert2";
 
 export const CadastroEmpresa = () => {
-    return (
-        <>
-            <div className="img2">
-                <img src="../assets/ONDA - CADASTRO.svg" alt="" />
-            </div>
-            <header className='header4'>
-                <div className="componente">
-                    <img src="../assets/LogoVitai.png" alt="Vitae" />
-                    <a href="">Home</a>
-                </div>
+  const [cep, setCep] = useState("");
+  const [numero, setNumero] = useState("");
+  const [logradouro, setLogradouro] = useState("");
+  const [complemento, setComplemento] = useState("");
 
-                <div className="nextPage3">
-                    <div>
-                        <h3>Já tem conta ?</h3>
-                        <h3>Então vamos logar!</h3>
-                    </div>
+  const navegando = useNavigate();
+  const inputPasswordRef = useRef<HTMLInputElement>(null);
 
-                    <button className="btn logar bold-20">
-                        Logar
-                        <img src="../assets/BackArrow.png" alt="" />
-                    </button>
-                </div>
-            </header>
+  const handleClickNav = () => {
+    if (cep === "" ||
+        numero === "" ||
+        logradouro === "") {
+          
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Preencha todos os campos obrigatórios",
+      });
+    }
 
-            <div className="container">
-                <div className="formulario">
-                    <div className="cadastro">
-                        <h1>Cadastre-se</h1>
-                        <h1 className="text-title">Bem vindo!!</h1>
-                        <h1 className="text-title">Cadastre as informações do homocentro</h1>
-                    </div>
-                    <div className="inputs">
-                        <input className="input-size" type="text" placeholder="CEP" />
-                        <input className="input-size" type="text" placeholder="Número" />
-                        <input className="input-size" type="text" placeholder="Logradouro" />
-                        <input className="input-size" type="text" placeholder="Complemento" />
-                        <div className="button">
-                            <button className="btn cadastrar bold-20" >
-                                Cadastrar
-                                <img src="../assets/BackArrow.png" alt="" />
-                            </button>
+    else if (cep.length < 8 || cep.length > 8) {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "CEP invalído",
+      });
+    }
 
-                        </div>
-                    </div>
-                </div>
-                <img className="imgDoe" src="../assets/ilustracao-aos-bancos-de-sangue-reduzida 3.png" alt="" />
-            </div>
-        </>
-    );
-}
+    else if (numero.length << 0) {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Valores negativos inseridos",
+      });
+    }
+
+    else if (/[^a-zA-Z0-9\s]/.test(logradouro)) {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Logradouro não pode conter caracteres especiais",
+      });
+    } 
+
+    else {
+      navegando("/login");
+    }
+  };
+
+  const handleClickNav2 = () => {
+    navegando("/login");
+  };
+
+  return (
+    <>
+      <div className="img">
+        <img className="onda1" src={vetorImg[4]} alt="" />
+        <img className="imgDoe" src={vetorImg[1]} alt="" />
+      </div>
+
+      <header className="header1">
+        <div className="componente1">
+          <img src={vetorImg[3]} alt="Vitae" />
+          <a className="roboto bold-20" href="/pagina-inicial">
+            Home
+          </a>
+        </div>
+
+        <div className="nextPage">
+          <div>
+            <h3>Já tem conta ?</h3>
+            <h3>Então vamos logar!</h3>
+          </div>
+
+          <button onClick={handleClickNav2} className="btn logar bold-20">
+            Logar
+            <img src={vetorIcon[0]} alt="" />
+          </button>
+        </div>
+      </header>
+
+      <div className="container1">
+        <div className="formulario1">
+          <div className="cadastro">
+            <h1>Cadastre-se</h1>
+            <h1 className="text-title">Bem vindo!!</h1>
+            <h1 className="text-title">
+              Cadastre as informações do Hemocentro
+            </h1>
+          </div>
+          <div className="inputs">
+            <Input
+              className={"input-size"}
+              placeholder={"CEP"}
+              type="number"
+              value={cep}
+              onChange={(newValue) => setCep(newValue)}
+              onPressEnter={() => inputPasswordRef.current?.focus()}
+            />
+            <Input
+              className={"input-size"}
+              placeholder={"Número"}
+              type="number"
+              value={numero}
+              ref={inputPasswordRef}
+              onChange={(newValue) => setNumero(newValue)}
+              onPressEnter={() => inputPasswordRef.current?.focus()}
+            />
+            <Input
+              className={"input-size"}
+              placeholder={"Logradouro"}
+              value={logradouro}
+              ref={inputPasswordRef}
+              onChange={(newValue) => setLogradouro(newValue)}
+            />
+            <Input
+              className={"input-size"}
+              placeholder={"Complemento"}
+              value={complemento}
+              ref={inputPasswordRef}
+              onChange={(newValue) => setComplemento(newValue)}
+            />
+          </div>
+          <div className="button">
+            <button onClick={handleClickNav} className="btn cadastrar bold-20">
+              Cadastrar
+              <img src={vetorIcon[0]} alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- site/src/app/pages/funcionario/cadastro-funcionario/CadastroFuncionario.tsx
+++ site/src/app/pages/funcionario/cadastro-funcionario/CadastroFuncionario.tsx
@@ -1,52 +1,268 @@
-import '../../../../html-css-template/css/novoFuncionario.css'
+import "../../../../html-css-template/css/novoFuncionario.css";
+
+import { vetorImg } from "../../../shared/components/imagens";
+import { vetorIcon } from "../../../shared/components/imagens";
+
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { Input } from "../../../shared/components";
+
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 export const CadastroFuncionario = () => {
-    return (
-        <>
-            <div className="menu">
-                <h1 className="rowdies bold-30">Olá,Pedro!</h1>
-                <div className="menuItens">
-                    <a href="" className="now roboto sbold-20">Cadastro Funcionario</a>
-                    <a href="" className="item roboto sbold-20">Requisitar Doação</a>
-                    <a href="" className="item roboto sbold-20">Cadastrar Doação</a>
-                </div>
-                <button className="btn bg-vermelhoClaro">Sair</button>
-            </div>
-            <div className="conteudo">
-                <div className="rowdies topo">
-                    <div className="titulo">
-                        <h1>NOVO FUNCIONARIO</h1>
-                    </div>
+  const [email, setEmail] = useState("");
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confSenha, setConfSenha] = useState("");
 
-                    <div className="logo">
-                        <img src="../assets/LogoVitai.png" alt="" />
-                    </div>
-                </div>
-                <div className="ondas">
-                    <div className="waves">
-                        <img src="../assets/ONDA - MAPA.svg" alt="" />
-                    </div>
-                </div>
-                <div className="containerCadastro">
-                    <div className="cadastroFuncionario roboto">
-                        <div className="cadastroInputs">
-                            <input type="text" placeholder="Email" />
-                            <input type="text" placeholder="Nome Completo" />
-                            <input placeholder="Senha" />
-                            <input type="text" placeholder="Cargo" />
-                            <div className="confirmaSenha">
-                                <input id="input" placeholder="Confirma senha" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="btnCadastrar btn">
-                        Cadastrar
-                        <span
-                        //<i className="gg-arrow-right-o">  <svg><use xlink:href="/all.svg#gg-arrow-right-o"/></svg></i>
-                        ></span>
-                    </div>
-                </div>
+  sessionStorage.setItem("email", email.trim());
+  sessionStorage.setItem("senha", senha.trim());
+  sessionStorage.setItem("confSenha", confSenha);
+
+  const navegando = useNavigate();
+  const inputPasswordRef = useRef<HTMLInputElement>(null);
+
+  const handleClickNav = () => {
+    if (email === "" || nome === "" || cargo === "" || senha === "" || confSenha === "") {
+
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Preencha todos os campos obrigatórios",
+      });
+
+    }
+
+    else if (!email.includes("@")) {
+      
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Email precisa conter @",
+      });
+
+    }
+
+    else if (
+      !email.includes("outlook.com") &&
+      !email.includes("hotmail.com") &&
+      !email.includes("gmail.com") &&
+      !email.includes("yahoo.com") &&
+      !email.includes("icloud.com")
+    ) {
+      
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Insira um dominio valido",
+      });
+
+    }
+
+    else if (senha === "") if (senha.length < 8) {
+        
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Digite uma senha forte",
+      });
+    }
+
+    
+    else if (!/\s/.test(nome) && nome.length < 15) {
+      
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Digite o nome completo",
+      });
+    }
+
+    else if (nome.length > 80) {
+      
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Nome incorreto",
+      });
+    }
+
+    else if (confSenha === "") {
+      
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Confirmar sua senha"
+      });
+    }
+
+    else if (confSenha === senha) {
+      navegando("/perfil-funcionario/cadastro-funcionario");
+    }
+  };
+
+  const handleClickNav2 = () => {
+    navegando("/pagina-inicial");
+  };
+
+  return (
+    <>
+      <div className="geral">
+        <div className="menu">
+          <h1 className="rowdies bold-30">Olá,Pedro!</h1>
+          <div className="menuItens">
+            <a href="" className="now roboto sbold-20">
+              Cadastro Funcionario
+            </a>
+            <a href="" className="item roboto sbold-20">
+              Requisitar Doação
+            </a>
+            <a href="" className="item roboto sbold-20">
+              Cadastrar Doação
+            </a>
+          </div>
+          <button onClick={handleClickNav2} className="btn bg-vermelhoClaro">
+            Sair
+          </button>
+        </div>
+        <div className="conteudo">
+          <div className="rowdies topo">
+            <div className="titulo">
+              <h1>NOVO FUNCIONARIO</h1>
             </div>
-        </>
-    );
-}
+
+            <div className="logo">
+              <img src={vetorImg[3]} alt="" />
+            </div>
+          </div>
+          <div className="ondas">
+            <div className="waves">
+              <img src={vetorImg[6]} alt="" />
+            </div>
+          </div>
+          <div className="containerCadastro">
+            <div className="cadastroFuncionario roboto">
+              <div className="cadastroInputs">
+                <Input
+                  className={"input-size"}
+                  type="text"
+                  placeholder={"Nome Completo"}
+                  value={nome}
+                  ref={inputPasswordRef}
+                  onChange={(newValue) => setNome(newValue)}
+                />
+                <Input
+                  className={"input-size"}
+                  type="text"
+                  placeholder={"Cargo"}
+                  value={cargo}
+                  ref={inputPasswordRef}
+                  onChange={(newValue) => setCargo(newValue)}
+                />
+                <Input
+                  className={"input-size"}
+                  type="text"
+                  placeholder={"Email"}
+                  value={email}
+                  ref={inputPasswordRef}
+                  onChange={(newValue) => setEmail(newValue)}
+                />
+                <Input
+                  className={"input-size"}
+                  placeholder={"Senha"}
+                  value={senha}
+                  onChange={(newValue) => setSenha(newValue)}
+                />
+                <div className="confirmaSenha">
+                  <Input
+                    className={"input-size"}
+                    type="text"
+                    placeholder={"Confirma senha"}
+                    value={confSenha}
+                    ref={inputPasswordRef}
+                    onChange={(newValue) => setConfSenha(newValue)}
+                  />
+                </div>
+              </div>
+            </div>
+            <button onClick={handleClickNav} className="btnCadastrar btn">
+              Cadastrar
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
