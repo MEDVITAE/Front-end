67cf3f808b36084e78b2bfb78809388dca664792
willianpaternezi <willian.paternezi@sptech.school>
2023-11-24 16:09:08 -0300

    validações e insercação de dados no banco

--- site/src/app/pages/cadastro/CadastroDados.tsx
+++ site/src/app/pages/cadastro/CadastroDados.tsx
@@ -1,13 +1,12 @@
 import '../../../html-css-template/css/dados.css'
-
+import { useCallback, useState } from 'react';
 import { vetorImg } from '../../shared/components/imagens';
 import { vetorIcon } from '../../shared/components/imagens';
-
-import { useCallback, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-import { TarefasService } from '../../shared/sevice/api/tarefas/TarefasService';
+import { Await, useNavigate } from 'react-router-dom';
+import { IPrimeiroCadastro, TarefasService } from '../../shared/sevice/api/tarefas/TarefasService';
 import { Input } from '../../shared/components';
+import { ApiException } from '../../shared/sevice/api/ApiException';
+import Swal from "sweetalert2";
 
 export const CadastroDados = () => {
     const navegando = useNavigate();
@@ -15,35 +14,195 @@ export const CadastroDados = () => {
     const [nome, setNome] = useState('');
     const [cpf, setCpf] = useState('');
     const [cep, setCep] = useState('');
-    const [numero, setNumero] = useState('');
+    const [numero, setNumero] = useState('') ;
     const [dtNascimento, setDtNascimento] = useState('');
     const [sexo, setSexo] = useState('');
-    const [sangue, setSangue] = useState('');
-    const [tel, setTel] = useState('');
+    const [checkCampanhas, setCheckCampanhas] = useState(false);
 
-    sessionStorage.getItem('sein');
-    sessionStorage.getItem('sei');
-    sessionStorage.getItem('se');
+    const [nomeError, setNomeError] = useState('');
+    const [cpfError, setCpfError] = useState('');
+    const [cepError, setCepError] = useState('');
+    const [numeroError, setNumeroError] = useState('');
+    const [dtNascimentoError, setDtNascimentoError] = useState('');
+    const [sexoError, setSexoError] = useState('');
 
-    const cadastrando = useCallback(() => {
-        console.log(sessionStorage.getItem('sein'));
-        console.log(sessionStorage.getItem('sei'));
-        console.log(sessionStorage.getItem('se'));
+    const validateFields = () => {
+        if (
+            nome === "" ||
+            cpf === "" ||
+            cep === "" ||
+            numero === "" ||
+            dtNascimento === "" ||
+            sexo === "" 
+          ) {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+              },
+            });
+            Toast.fire({
+              icon: "error",
+              title: "Preencha todos os campos obrigatórios",
+            });
+          }
+        else if (!nome.trim()) {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+              },
+            });
+            Toast.fire({
+              icon: "error",
+              title: "Nome é obrigatório",
+            });
+          }
+    
+        else if (!/^\d{11}$/.test(cpf)) {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+              },
+            });
+            Toast.fire({
+              icon: "error",
+              title: "CPF inválido, inserir apenas números",
+            });
+          }
+    
+        else if (!/^\d{8}$/.test(cep)) {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+              },
+            });
+            Toast.fire({
+              icon: "error",
+              title: "CEP inválido, inserir apenas números",
+            });
+          }
+    
+        else if (!/^\d+$/.test(numero)) {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+              },
+            });
+            Toast.fire({
+              icon: "error",
+              title: "Número inválido",
+            });
+          }
 
-        TarefasService.create({
+        //cadastrando com padrão data 00/00/0000 porque a varialvél está retornando String
+        else if  (!/^(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[0-2])\d{4}$/.test(dtNascimento)) {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+              },
+            });
+            Toast.fire({
+              icon: "error",
+              title: "Data de nascimento inválida, inserir apenas numeros",
+            });
+          }
+
+        else if (/^[Mm][Aa]?[Ss]?[Cc]?[Uu]?[Ll]?[Ii]?[Nn]?[Oo]$/.test(sexo) && /^[Ff][Ee]?[Mm]?[Ii]?[Nn]?[Ii]?[Nn]?[Oo]$/.test(sexo)) {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+              },
+            });
+            Toast.fire({
+              icon: "error",
+              title: "Sexo inválido,",
+            });
+          }
+        
+        else {
+        return true;
+        }
+    };
+
+    const cadastrando = useCallback(async () => {
+       if (validateFields()){
+
+        const usuario: IPrimeiroCadastro | ApiException = await TarefasService.createUsuario({
             nome: nome,
-            email: sessionStorage.getItem('sein'),
-            senha: sessionStorage.getItem('sei'),
+            email: sessionStorage.getItem('email'),
+            senha: sessionStorage.getItem('senha'),
             role: 'PACIENTE',
-            cpf: cpf
+            cpf: cpf,
         });
+        const dia = dtNascimento.substring(0, 2);
+        const mes = dtNascimento.substring(2, 4);
+        const ano = dtNascimento.substring(4, 8);
+
+        const nascimento = ano + "-" + mes + "-" + dia;  
+
+        if(usuario instanceof ApiException){
+            return alert (usuario.message)
+        }else {
+            TarefasService.createUsuarioCaracteristicas({
+                peso :'',
+                altura : '',
+                tatto: false,
+                sexo: sexo,
+                nascimento: nascimento,
+                apto: false,
+                fkUsuario: usuario.id,
+            })
 
         sessionStorage.clear();
-    }, []);
+            }
+        }
+    }, [nome,cpf,cep,numero,dtNascimento,sexo]);
 
-    const navegarClick = () => {
-        //navegando("/login");
+    const navegarClick = async () => {
         cadastrando();
+        await new Promise(result => setTimeout(result, 2000));
+        navegando("/login");
     }
 
     return (
@@ -65,46 +224,62 @@ export const CadastroDados = () => {
                         <h1 className="text-title">Cadastre os demais dados pessoais</h1>
                     </div>
                     <div className="inputsDados">
-                        <div className="input-right">
+                        <div className="input-left">
+                            {nomeError && <div className="error-message">{nomeError}</div>}
                             <Input
-                                className={"input-size"}
-                                placeholder={"Nome completo"}
-
-                                value={nome}
-                                onChange={newValue => setNome(newValue)}
-                            />
-                            <Input
-                                className={"input-size"}
+                                className={`input-size ${cpfError ? 'error' : ''}`}
                                 placeholder={"CPF"}
-
                                 value={cpf}
                                 onChange={newValue => setCpf(newValue)}
                             />
+                            {cpfError && <div className="error-message">{cpfError}</div>}
                             <Input
-                                className={"input-size"}
+                                className={`input-size ${cepError ? 'error' : ''}`}
                                 placeholder={"CEP"}
-
                                 value={cep}
                                 onChange={newValue => setCep(newValue)}
                             />
+                            {cepError && <div className="error-message">{cepError}</div>}
                             <Input
                                 className={"input-size"}
                                 placeholder={"Número"}
-
                                 value={numero}
                                 onChange={newValue => setNumero(newValue)}
                             />
                         </div>
-                        <div className="input-left">
-                            <input className="input-size" type="text" placeholder="Data de nascimento" />
-                            <input className="input-size" type="text" placeholder="Sexo biologico" />
-                            <input className="input-size" type="text" placeholder="Tipo sanguineo" />
-                            <input className="input-size" type="text" placeholder="Telefone" />
+                        <div className="input-right">
+                        <Input
+                                className={`input-size ${nomeError ? 'error' : ''}`}
+                                placeholder={"Nome completo"}
+                                value={nome}
+                                onChange={newValue => setNome(newValue)}
+                            />
+                            <Input
+                                className="input-size"
+                                type="text"
+                                placeholder="Data de nascimento"
+                                value={dtNascimento}
+                                onChange={(newValue) => setDtNascimento(newValue)}
+                            />
+                            <Input
+                                className="input-size"
+                                type="text"
+                                placeholder="Sexo biológico"
+                                value={sexo}
+                                onChange={(newValue) => setSexo(newValue)}
+                            />
                         </div>
                     </div>
                     <div className="check">
-                        <input className="minhaCaixaDeSelecao" type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao"
-                            value="valorDaCaixaDeSelecao" />
+                        <input 
+                            className="minhaCaixaDeSelecao" 
+                            type="checkbox" 
+                            id="minhaCaixaDeSelecao" 
+                            name="minhaCaixaDeSelecao"
+                            value="valorDaCaixaDeSelecao"
+                            checked={checkCampanhas} 
+                            onChange={() => setCheckCampanhas(!checkCampanhas)}
+                            />
                         <h3>Desejo ser informado de campanhas e doações de emergencia ?</h3>
                     </div>
                     <button onClick={navegarClick} className="btn cadastrar bold-20">
--- site/src/app/shared/sevice/api/tarefas/TarefasService.ts
+++ site/src/app/shared/sevice/api/tarefas/TarefasService.ts
@@ -1,13 +1,32 @@
 import { Api } from "../ApiConfig";
 import { ApiException } from "../ApiException";
 
-export interface ITarefa {
+export interface IPrimeiroCadastro {
     id: number;
     nome: string;
     email: string | null;
     senha: string | null;
     role: string;
     cpf: string;
+}
+
+export interface ISegundoCadastroEndereco{
+    cidade: string;
+    bairro: string;
+    cep: string;
+    logradouro: string;
+    rua: string;
+    numero: number;
+    fkUsuario: number;
+}
+export interface ISegundoCadastroCaracteristicas{
+    peso :string;
+    altura : string;
+    tatto: boolean;
+    sexo: string;
+    nascimento: string;
+    apto: boolean;
+    fkUsuario: number;
 }
 
 export interface IListagemHemocentro {
@@ -46,7 +65,7 @@ type THistoricoAgendamento = {
     data: IHistoricoAgendamento[];
 }
 
-const getAll = async (): Promise<ITarefa[] | ApiException> => {
+const getAll = async (): Promise<IPrimeiroCadastro[] | ApiException> => {
     try {
         const { data } = await Api().get('/swagger-ui.html');
         return data;
@@ -114,7 +133,7 @@ const getAllHospital = async (filter = ''): Promise<THemocentroComTotalCount | E
 
 
 
-const getById = async (id: number): Promise<ITarefa | ApiException> => {
+const getById = async (id: number): Promise<IPrimeiroCadastro | ApiException> => {
     try {
         const { data } = await Api().get(`/tarefas/${id}`);
         return data;
@@ -140,7 +159,7 @@ const getByIdHistoricoAgendamentoAtual = async (id: number): Promise<IHistoricoA
 };
 
 //Criar outro método para inserção de dados
-const create = async (dataToCreate: Omit<ITarefa, 'id'>): Promise<ITarefa | ApiException> => {
+const createUsuario = async (dataToCreate: Omit<IPrimeiroCadastro, 'id'>): Promise<IPrimeiroCadastro | ApiException> => {
     try {
         const { data } = await Api().post<any>('/usuario/register', dataToCreate);
         return data;
@@ -151,7 +170,29 @@ const create = async (dataToCreate: Omit<ITarefa, 'id'>): Promise<ITarefa | ApiE
 
 };
 
-const updateById = async (id: number, dataToUpdate: ITarefa): Promise<ITarefa | ApiException> => {
+const createUsuarioEndereco = async (dataToCreate: ISegundoCadastroEndereco): Promise<ISegundoCadastroEndereco | ApiException> => {
+    try {
+        const { data } = await Api().post<any>('/Endereco', dataToCreate);
+        return data;
+    }
+    catch (error: any) {
+        return new ApiException(error.message || 'Erro ao criar registro.');
+    }
+
+};
+
+const createUsuarioCaracteristicas = async (dataToCreate: ISegundoCadastroCaracteristicas): Promise<ISegundoCadastroCaracteristicas | ApiException> => {
+    try {
+        const { data } = await Api().post<any>('/Caracteristicas', dataToCreate);
+        return data;
+    }
+    catch (error: any) {
+        return new ApiException(error.message || 'Erro ao criar registro.');
+    }
+
+};
+
+const updateById = async (id: number, dataToUpdate: IPrimeiroCadastro): Promise<IPrimeiroCadastro | ApiException> => {
     try {
         const { data } = await Api().put(`/tarefas/${id}`, dataToUpdate);
         return data;
@@ -189,7 +230,9 @@ export const TarefasService = {
     getAllHospital,
     getById,
     getByIdHistoricoAgendamentoAtual,
-    create,
+    createUsuario,
+    createUsuarioEndereco,
+    createUsuarioCaracteristicas,
     updateById,
     deleteById,
     deleteByIdAgedamento,
