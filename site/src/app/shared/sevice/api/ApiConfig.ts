c7fb628f5da487546bcd6dc6e100003dde8de6a5
MaessioSptech <maessio.sousa@sptech.school>
2024-03-21 18:06:32 -0300

    teste

--- site/src/app/shared/sevice/api/ApiConfig.ts
+++ site/src/app/shared/sevice/api/ApiConfig.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const Api = () => {
     return axios.create({
-        baseURL: 'http://54.81.60.55:8082/'
+        baseURL: 'http://54.227.65.243:8082/'
     });
 }
