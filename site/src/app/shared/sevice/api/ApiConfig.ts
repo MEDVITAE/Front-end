8dc02eddcb936a72ead3ea3873c4151260d6539b
export <114589490+joao25082001@users.noreply.github.com>
2024-03-21 16:07:16 -0300

    Update ApiConfig.ts

--- site/src/app/shared/sevice/api/ApiConfig.ts
+++ site/src/app/shared/sevice/api/ApiConfig.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const Api = () => {
     return axios.create({
-        baseURL: 'http://localhost:8082/'
+        baseURL: 'http://18.205.233.220:8082/'
     });
 }
