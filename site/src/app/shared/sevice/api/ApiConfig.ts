c696fdcf454a653ee43691531debc369690bdf4d
export <114589490+joao25082001@users.noreply.github.com>
2024-03-21 12:11:06 -0300

    Update ApiConfig.ts

--- site/src/app/shared/sevice/api/ApiConfig.ts
+++ site/src/app/shared/sevice/api/ApiConfig.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const Api = () => {
     return axios.create({
-        baseURL: 'http://3.87.119.102:8082'
+        baseURL: 'http://54.167.33.183:8082'
     });
 }
